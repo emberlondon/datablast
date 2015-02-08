f382079ef4a09db24890f734090ecd25b6a3c1fb
Jamie White <jamie@jgwhite.co.uk>
2015-02-08 18:29:48 +0000

    Add injected properties

--- app/router.js
+++ app/router.js
@@ -26,6 +26,7 @@ Router.map(function() {
   this.route('one-ten-upgrading');
   this.route('chained-else');
   this.route('block-params');
+  this.route('injected-properties');
 });
 
 export default Router;
