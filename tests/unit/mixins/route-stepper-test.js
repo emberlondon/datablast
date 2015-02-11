d9e26fddb379d7afe1edcf161b8caa13e6b87cba
Jamie White <jamie@jgwhite.co.uk>
2015-02-11 23:16:10 +0000

    ember init for 0.1.13

--- tests/unit/mixins/route-stepper-test.js
+++ tests/unit/mixins/route-stepper-test.js
@@ -1,10 +1,11 @@
 import Ember from 'ember';
 import RouteStepperMixin from '../../../mixins/route-stepper';
+import { module, test } from 'qunit';
 
-QUnit.module('RouteStepperMixin');
+module('RouteStepperMixin');
 
 // Replace this with your real tests.
-QUnit.test('it works', function(assert) {
+test('it works', function(assert) {
   var RouteStepperObject = Ember.Object.extend(RouteStepperMixin);
   var subject = RouteStepperObject.create();
   assert.ok(subject);
--- tests/unit/utils/preload-images-test.js
+++ tests/unit/utils/preload-images-test.js
@@ -1,9 +1,10 @@
 import preloadImages from '../../../utils/preload-images';
+import { module, test } from 'qunit';
 
-QUnit.module('preloadImages');
+module('preloadImages');
 
 // Replace this with your real tests.
-QUnit.test('it works', function(assert) {
+test('it works', function(assert) {
   var result = preloadImages();
   assert.ok(result);
 });
