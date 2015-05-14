43324bdc6ea487360fc9f0b1704945285d3107d8
Jamie White <jamie@jgwhite.co.uk>
2015-05-14 13:50:20 +0100

    Fix db-slide-test

--- tests/unit/components/db-slide-test.js
+++ tests/unit/components/db-slide-test.js
@@ -4,8 +4,7 @@ import {
 } from 'ember-qunit';
 
 moduleForComponent('db-slide', {
-  // specify the other units that are required for this test
-  // needs: ['component:foo', 'helper:bar']
+  needs: ['component:db-slide-container']
 });
 
 test('it renders', function(assert) {
