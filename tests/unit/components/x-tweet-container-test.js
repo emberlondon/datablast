82f6a36bd49ab17a44ba4c95c88b284342b1e918
Jamie White <jamie@jgwhite.co.uk>
2015-05-14 13:50:31 +0100

    ember g component x-tweet-container

--- tests/unit/components/x-tweet-container-test.js
+++ tests/unit/components/x-tweet-container-test.js
@@ -3,18 +3,19 @@ import {
   test
 } from 'ember-qunit';
 
-moduleForComponent('x-tweet', {
-  needs: ['component:x-tweet-container']
+moduleForComponent('x-tweet-container', {
+  // Specify the other units that are required for this test
+  // needs: ['component:foo', 'helper:bar']
 });
 
 test('it renders', function(assert) {
   assert.expect(2);
 
-  // creates the component instance
+  // Creates the component instance
   var component = this.subject();
   assert.equal(component._state, 'preRender');
 
-  // renders the component to the page
+  // Renders the component to the page
   this.render();
   assert.equal(component._state, 'inDOM');
 });
