5b67d4968955f94dd7b0afdf9fd5df37557b1c8a
Nik Wakelin <me@nikwakelin.com>
2015-05-27 10:48:26 +0100

    ember install ember-prism

--- Brocfile.js
+++ Brocfile.js
@@ -2,7 +2,14 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp();
+var app = new EmberApp({
+
+  'ember-prism': {
+    'theme': 'okaidia',
+    'components': ['handlebars', 'javascript'],
+  }
+
+});
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
