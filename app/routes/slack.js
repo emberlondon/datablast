a2164d1faea109e78ac41e822b797d58b69a68b4
Jamie White <jamie@jgwhite.co.uk>
2015-06-10 08:27:01 +0100

    Update slack slide

--- app/routes/slack.js
+++ app/routes/slack.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
+import preloadImages from '../utils/preload-images';
 
 export default Ember.Route.extend({
+  preload: function() {
+    return preloadImages('/images/slack.png');
+  },
+
+  beforeModel: function() {
+    return this.preload();
+  }
 });
