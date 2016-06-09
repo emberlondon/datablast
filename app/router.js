8b9fc5882cc65c2d29322f8954b6599fc0bbebe3
Jamie White <jamie@jgwhite.co.uk>
2016-06-09 16:42:11 +0100

    2016-06

--- app/router.js
+++ app/router.js
@@ -15,7 +15,6 @@ Router.map(function() {
   this.route('roster');
   this.route('embercamp');
   this.route('emberfest');
-  this.route('simplabs');
   this.route('zoey');
   this.route('on-to-the-talks');
 });
