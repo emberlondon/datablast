32a2ddaa7643b46a4d31407809a026f21010ccba
Michael Schinis <mschinis@gmail.com>
2016-09-08 17:34:15 +0100

    fix(index): fix month

--- app/router.js
+++ app/router.js
@@ -13,9 +13,9 @@ Router.map(function() {
   this.route('ember-london');
   this.route('ember-london-stats');
   this.route('upcoming-events');
-  this.route('website');
-  this.route('slack');
-  this.route('datablast');
+  // this.route('website');
+  // this.route('slack');
+  // this.route('datablast');
   // this.route('glimmer-2');
   this.route('on-to-the-talks');
 });
